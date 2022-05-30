cb5361c4edc5f4e9f2d9d6d00a1f99d21f9d7326
Gilberto Garcia <gilbertogjn@gmail.com>
2022-05-30 09:26:30 -0400

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+
+export default function Navbar() {
+    return (
+        <navbar>
+            <img src={logo} alt='Airbnb Logo' />
+        </navbar>
+    )
+}
